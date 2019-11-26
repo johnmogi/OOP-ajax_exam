44be4226abbebfd878fd368868da7043a096e5fd
john <johnmogi>
2019-11-26 18:42:33 +0200

    almost finished - lacking diameter

--- HW/2/main.js
+++ HW/2/main.js
@@ -16,10 +16,8 @@ var Shape = /** @class */ (function () {
         this.name = name;
         this.color = color;
     }
-    Shape.prototype.display = function () {
-        console.log("name :" + this.name);
-        console.log("color :" + this.color);
-    };
+    Shape.prototype.display = function () { };
+    ;
     return Shape;
 }());
 var Rectangular = /** @class */ (function (_super) {
@@ -31,32 +29,62 @@ var Rectangular = /** @class */ (function (_super) {
     }
     Rectangular.prototype.display = function () {
         _super.prototype.display.call(this);
-        console.log("edge :" + this.edge);
+        // console.log("edge :"+ this.edge)
     };
     Rectangular.prototype.getArea = function (height, length) {
         var area = (2 * height) + (2 * length);
-        console.log("the Area of " + this.name + " is " + area);
+        console.log("the Area of this rect:" + this.name + " is " + area);
         return area;
     };
     Rectangular.prototype.getPerimeter = function (height, length) {
         var perimeter = height * length;
-        console.log("the perimeter of " + this.name + " is " + perimeter);
+        console.log("the perimeter of this rect: " + this.name + " is " + perimeter);
         return perimeter;
     };
     return Rectangular;
 }(Shape));
+var Circle = /** @class */ (function (_super) {
+    __extends(Circle, _super);
+    function Circle(name, color, radius) {
+        var _this = _super.call(this, name, color) || this;
+        _this.radius = radius;
+        return _this;
+    }
+    Circle.prototype.display = function () {
+        _super.prototype.display.call(this);
+        // console.log("radius :"+ this.radius)
+    };
+    Circle.prototype.getArea = function (height, length) {
+        var area = (2 * height) + (2 * length);
+        console.log("the Area of this circle is: " + this.name + " is " + area);
+        return area;
+    };
+    Circle.prototype.getPerimeter = function (height, length) {
+        var perimeter = height * length;
+        console.log("the perimeter of this circle is: " + perimeter);
+        return perimeter;
+    };
+    return Circle;
+}(Shape));
 var shapeArr = [];
-shapeArr.length = 19;
+shapeArr.length = 20;
 // shapeArr[0] = new Rectangular("box1","blue",10)
 for (var i = 0; i < shapeArr.length; i++) {
-    var rand1 = Math.floor(Math.random() * Math.floor(201));
-    var rand2 = Math.floor(Math.random() * Math.floor(201));
-    shapeArr.push(new Rectangular("shape" + rand1 * rand2, "blue", 10));
-    for (var _i = 0, shapeArr_1 = shapeArr; _i < shapeArr_1.length; _i++) {
-        var item = shapeArr_1[_i];
-        if (item instanceof Rectangular) {
-            shapeArr[i].getArea(rand1, rand2);
-            shapeArr[i].getPerimeter(rand1, rand2);
-        }
+    var rand1 = Math.floor(Math.random() * Math.floor(100));
+    var rand2 = Math.floor(Math.random() * Math.floor(100));
+    // let's say rand 1 is the rect side
+    if (rand1 > 49) {
+        shapeArr[i] = new Rectangular("shape" + rand1 * rand2, "blue", rand1);
+        console.log(i, shapeArr[i].getArea(rand1, rand2));
+        console.log(i, shapeArr[i].getPerimeter(rand1, rand2));
     }
+    // let's assume rand2 is the circle radius
+    else {
+        shapeArr[i] = new Circle("shape" + rand1 * rand2, "red", rand2);
+        console.log(i, shapeArr[i].getArea(rand1, rand2));
+        console.log(i, shapeArr[i].getPerimeter(rand1, rand2));
+    }
+}
+for (var _i = 0, shapeArr_1 = shapeArr; _i < shapeArr_1.length; _i++) {
+    var item = shapeArr_1[_i];
 }
--- HW/2/main.ts
+++ HW/2/main.ts
@@ -8,11 +8,7 @@ abstract class Shape{
         this.color= color;
        
     }
-    public display() :void{
-        console.log("name :"+ this.name);
-        console.log("color :"+ this.color);
-
-    }
+    public display() :void{};
     public abstract getArea(height:number,length:number):number;
     public abstract getPerimeter(height:number,length:number): number;
     }
@@ -25,35 +21,66 @@ abstract class Shape{
         }
         public display() :void{
             super.display();
-            console.log("edge :"+ this.edge)
+            // console.log("edge :"+ this.edge)
         }
         public getArea(height:number,length:number):number{
 
             let area = (2*height)+ (2*length)
-            console.log("the Area of " + this.name + " is " + area)
+            console.log("the Area of this rect:" + this.name + " is " + area)
             return area
         }
         public getPerimeter(height:number,length:number):number{
             let perimeter = height *length
-            console.log("the perimeter of " + this.name + " is " + perimeter)
+            console.log("the perimeter of this rect: " + this.name + " is " + perimeter)
             return perimeter
         }
     }
+   
+    class Circle extends Shape{
+        public radius:number
+        public constructor(name: string,color:string, radius:number){
+            super(name,color)
+            this.radius = radius;
+        }
+        public display() :void{
+            super.display();
+            // console.log("radius :"+ this.radius)
+        }
+        public getArea(height:number,length:number):number{
 
+            let area = (2*height)+ (2*length)
+            console.log("the Area of this circle is: " + this.name + " is " + area)
+            return area
+        }
+        public getPerimeter(height:number,length:number):number{
+            let perimeter = height *length
+            console.log("the perimeter of this circle is: " + perimeter)
+            return perimeter
+        }
+    }
     let shapeArr: Shape[] = [];
-    shapeArr.length=19;
+    shapeArr.length=20;
 
     // shapeArr[0] = new Rectangular("box1","blue",10)
 
     for (let i = 0; i < shapeArr.length; i++) {
-        let rand1 = Math.floor(Math.random() * Math.floor(201));
-        let rand2 = Math.floor(Math.random() * Math.floor(201));
-        shapeArr.push(new Rectangular("shape" + rand1 * rand2 ,"blue",10))
+        let rand1 = Math.floor(Math.random() * Math.floor(100));
+        let rand2 = Math.floor(Math.random() * Math.floor(100));
 
-    for (let item of shapeArr){
-        if (item instanceof Rectangular){
-      shapeArr[i].getArea(rand1,rand2)
-      shapeArr[i].getPerimeter(rand1,rand2)
+    // let's say rand 1 is the rect side
+        if(rand1>49){ 
+        shapeArr[i] = new Rectangular("shape" + rand1 * rand2 ,"blue",rand1)
+        console.log(i,shapeArr[i].getArea(rand1,rand2))
+        console.log(i,shapeArr[i].getPerimeter(rand1,rand2))
+        }
+        // let's assume rand2 is the circle radius
+        else{
+            shapeArr[i] = new Circle("shape" + rand1 * rand2 ,"red",rand2)
+            console.log(i,shapeArr[i].getArea(rand1,rand2))
+            console.log(i,shapeArr[i].getPerimeter(rand1,rand2))    
         }
     }
-}
+
+        for (let item of shapeArr){
+
+        }
